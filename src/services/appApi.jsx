06c952d40f794969b84e91d26db2faa9ee2ba82e
Talharao777 <124665621+Talharao777@users.noreply.github.com>
2023-02-20 23:41:14 +0500

    Update appApi.jsx

--- src/services/appApi.jsx
+++ src/services/appApi.jsx
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://chat-app-v1.herokuapp.com/",
+        baseUrl: "https://chat-app-v1.herokuapp.com",
     }),
 
     endpoints: (builder) => ({
